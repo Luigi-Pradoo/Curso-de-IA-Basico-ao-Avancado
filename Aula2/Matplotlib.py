d0b71cb1fc5c346515ff6e219c567443fca3b1bd
Luigi Prado <luigiprado012@gmail.com>
2026-09-10 10:41:32 -0300

    Slide Aula 2

--- Aula2/Matplotlib.py
+++ Aula2/Matplotlib.py
@@ -1,14 +1,58 @@
 import matplotlib.pyplot as plt
 
-#Grafico de linha
+nomes = ["Evelyn", "Pedro", "Gustavo", "Mateus", "Felipe", "Marcelo"]
+faltas = [2, 5, 1, 3, 7, 4]
 
-faltas = [1, 2, 3, 4, 5]
-dias = [25, 27, 26, 30, 32]
+# Gráfico de barras
+plt.bar(nomes, faltas)
 
-plt.plot(faltas, dias)
-
-plt.xlabel("Faltas")
-plt.ylabel("Dias")
-plt.title("Dias de aula por número de faltas")
+plt.xlabel("Alunos")
+plt.ylabel("Dias de falta")
+plt.title("Faltas dos alunos")
 
 plt.show()
+
+
+#Grafico de linhas
+import matplotlib.pyplot as plt
+
+nomes = ["Evelyn", "Pedro", "Gustavo", "Mateus", "Felipe", "Marcelo"]
+faltas = [2, 5, 1, 3, 7, 4]
+
+plt.plot(nomes, faltas)
+
+plt.xlabel("Alunos")
+plt.ylabel("Dias de falta")
+plt.title("Faltas dos alunos")
+
+plt.show()
+
+#Histograma
+import matplotlib.pyplot as plt
+
+faltas = [2, 5, 1, 3, 7, 4]
+
+plt.hist(faltas)
+
+plt.xlabel("Dias de falta")
+plt.ylabel("Quantidade de alunos")
+plt.title("Distribuição das faltas")
+
+plt.show()
+
+#dispersao 
+
+import matplotlib.pyplot as plt
+
+aulas = [20, 21, 19, 22, 23, 24]
+faltas = [2, 5, 1, 3, 7, 4]
+
+plt.scatter(aulas, faltas)
+
+plt.xlabel("Aulas")
+plt.ylabel("Dias de falta")
+plt.title("Aulas x Dias de falta")
+
+plt.show()
+
+
